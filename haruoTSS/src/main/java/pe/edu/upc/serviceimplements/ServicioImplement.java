cc52d34978aab8423b29f79074e957a1bcbce9f9
HaruOC <haruooyola123@hotmail.com>
2021-11-09 22:18:33 -0500

    haruoTSS

---
+++ haruoTSS/src/main/java/pe/edu/upc/serviceimplements/ServicioImplement.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.serviceimplements;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.edu.upc.entities.Servicio;
+import pe.edu.upc.repositories.IServicioRepository;
+import pe.edu.upc.serviceinterfaces.IServicioService;
+
+@Service
+public class ServicioImplement implements IServicioService {
+
+	@Autowired
+	private IServicioRepository sR;
+
+	@Override
+	public boolean insert(Servicio servicio) {
+		Servicio objServicio = sR.save(servicio);
+		if (objServicio == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public List<Servicio> list() {
+		return sR.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Servicio> listarId(int CServicio) {
+		return sR.findById(CServicio);
+	}
+
+	@Override
+	public void delete(int idServicio) {
+		sR.deleteById(idServicio);
+	}
+
+}
